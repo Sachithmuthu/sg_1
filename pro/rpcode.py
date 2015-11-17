db9579b10bd4cddc305646aa2088533f89ea7c0a
Sachithmuthu <sachith@ahlab.org>
2015-11-17 19:19:03 +0000

    modified frame

--- pro/rpcode.py
+++ pro/rpcode.py
@@ -13,6 +13,7 @@ config=map(float,results)
 #assign extracted values for each variables
 [resX,resY,frameRate,minThresh,maxTresh,minArea,maxArea]=[  int(config[0]),int(config[1]),float(config[2]),float(config[3]), float(config[4]),float(config[5]),float(config[6])]
 [minCir,minConv,minInert,LR,threshMove,threshCount]=[float(config[7]),float(config[8]),float(config[9]),float(config[10]),float(config[11]),float(config[12]) ]
+[X_low,X_high,Y_low,Y_high]=[ int(config[13]),int(config[14]),int(config[15]),int(config[16])]
 
 #camera configuration
 camera = PiCamera()
@@ -56,6 +57,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
 	counter =0
 	move=0
+	counterB=0
 	frame = f.array
 	image = fgbg.apply(frame,learningRate=LR)	
 	image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
@@ -64,21 +66,22 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 	points = detector.detect(image)
 
 	#drawing circles on keypoints 
-        image_with_blobs = cv2.drawKeypoints(image, points, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        for kp in points :
-	    diameter=round(kp.size)
-	    #calculating movement 
-	    if diameter>threshMove:  								
-	    	move+=1
-	    #counting the people
-	    if diameter>threshCount:								
-		counter+=1
-	x=move*5
-	movement=min(x,100)
+        image_with_blobs = cv2.drawKeypoints(frame, points, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	
+	#calculations
+	for kp in points :
+        	x=round(kp.pt[0])
+        	y=round(kp.pt[1])
+		diameter=round(kp.size)
+        	if (X_low<x<X_high and Y_low<y<Y_high and diameter>threshCount ):
+            		counterB+=1
+		if diameter>threshMove:  								
+	    		move+=1
+       		print "[x=%d,y=%d]" %(x,y),
+	movement=min(move,100)
+    	
 	#display the values
-	print "Count=%d" %counter,
+	print "CountB=%d" %counterB,
 	print "Move=%d" %movement,
         print      
  
